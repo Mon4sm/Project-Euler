29ed7c34b6fa0c69866dadccf73f46d740ab9188
Monasm <111132208+Mon4sm@users.noreply.github.com>
2024-05-14 19:41:25 +0700

    Create solution.cpp

---
+++ 9# Special Pythagorean Triplet/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    for(n=1;n<=50;n++){
+        for(m=1;m<=50;m++){
+            if((m*m)+(m*n) == 500){
+                int c = (m*m)+(n*n);
+                int a = (m*m)-(n*n);
+                int b = 2*m*n;
+                if(a+b+c == 1000){
+                    cout <<a*b*c;
+                    return 0;
+                }
+            }
+        }
+    } 
+    return 0;
+}
